fadfb0f88fa0f3a853d1e42e9750531b08bde639
Anass NB <Anassnabil067@gmail.com>
2023-05-04 16:49:54 +0100

    solvre problem in profile

--- app/Models/Etudiant.php
+++ app/Models/Etudiant.php
@@ -74,10 +74,19 @@ class Etudiant extends Authenticatable
   //Check the student has enter all informations in his profile
   public function isProfileComplete()
   {
-    $requiredFields = ['nom', 'nom_ar', 'prenom', 'prenom_ar', 'code_massar', 'cin', 'lieu_naissance', 'lieu_naissance_ar','date_naissance','province_naissance',
-  'telephone','ville','sexe','adresse_perso3','adresse_perso2','adresse_perso1','situation_familiale','pays','email','fonctionnaire'];
-    $requiredFields = array_merge($requiredFields,'annee_obt_bac','moyenne_bac','serie_bac','province_bac','mention_bac','academie','type_diplome','etablissement');
+    $requiredFields = [
+      'nom', 'nom_ar', 'prenom', 'prenom_ar', 'code_massar', 'cin', 'lieu_naissance', 'lieu_naissance_ar', 'date_naissance', 'province_naissance','photo',
+      'telephone', 'ville', 'sexe', 'adresse_perso3', 'adresse_perso2', 'adresse_perso1', 'situation_familiale', 'pays', 'email', 'fonctionnaire'
+    ];
+    $dossierRequiredFields =  ['annee_obt_bac','annee_obt_diplome', 'moyenne_bac', 'serie_bac', 'province_bac', 'mention_bac', 'academie', 'type_diplome', 'etablissement','mention_diplome','moyenne_diplome',"note_s1",'note_s2','note_s3','note_s4','diplome_document','releve_note','bac_document','cv'];
+
     foreach ($requiredFields as $field) {
+      if (empty($this->$field)) {
+        return false;
+      }
+    }
+    //! Rah 0 ki3tabro database field khawi
+    foreach ($dossierRequiredFields as $field) {
       if (empty($this->$field)) {
         return false;
       }
@@ -90,19 +99,29 @@ class Etudiant extends Authenticatable
   // calculate the percentage of profile completion
   public function profileCompletionPercentage()
   {
-    $requiredFields = ['nom', 'nom_ar', 'prenom', 'prenom_ar', 'code_massar', 'cin', 'lieu_naissance', 'lieu_naissance_ar','date_naissance','province_naissance',
-    'telephone','ville','sexe','adresse_perso3','adresse_perso2','adresse_perso1','situation_familiale','pays','email','fonctionnaire'];
-      $requiredFields = array_merge($requiredFields,['annee_obt_bac','moyenne_bac','serie_bac','province_bac','mention_bac','academie','type_diplome','etablissement']);
+    $requiredFields = [
+      'nom', 'nom_ar', 'prenom', 'prenom_ar', 'code_massar', 'cin', 'lieu_naissance', 'lieu_naissance_ar', 'date_naissance', 'province_naissance','photo',
+      'telephone', 'ville', 'sexe', 'adresse_perso3', 'adresse_perso2', 'adresse_perso1', 'situation_familiale', 'pays', 'email', 'fonctionnaire'
+    ];
+    $dossierRequiredFields =  ['annee_obt_bac','annee_obt_diplome', 'moyenne_bac', 'serie_bac', 'province_bac', 'mention_bac', 'academie', 'type_diplome', 'etablissement','mention_diplome','moyenne_diplome',"note_s1",'note_s2','note_s3','note_s4','diplome_document','releve_note','bac_document','cv'];
 
-    $totalFields = count($requiredFields);
+    $totalFields = count($requiredFields) + count($dossierRequiredFields);
 
     $filledFields = 0;
 
 
     foreach ($requiredFields as $field) {
-      if (!empty($this->$field)) {
+      if (isset($this->$field)) {
+        $filledFields++;
+
+      }
+
+    }
+    foreach ($dossierRequiredFields as $field) {
+      if (isset($this->dossier->$field)) {
         $filledFields++;
       }
+
     }
 
     // Calculate the percentage of filled fields
@@ -128,5 +147,4 @@ class Etudiant extends Authenticatable
     }
     return $color;
   }
-
 }
